de6a28b2f2dea796a1b12e2024a0a2cea654f09f
Comnovis <oleksii.vakhnenko@comnovis.de>
2024-12-16 14:14:46 +0100

    update

--- UI/testui.py
+++ UI/testui.py
@@ -1,10 +1,15 @@
+import subprocess
 import tkinter as tk
 from datetime import datetime
 
 def on_button_click():
-    current_dateTime = datetime.now()
-    label.config(text=f"Hello World! {current_dateTime}")
- 
+    ps_command = f"Write-Host {str_var.get()}"
+    print(ps_run(ps_command))
+
+def ps_run(ps_command):
+    completed = subprocess.run(["powershell", "-Command", ps_command], capture_output=True)
+    return completed 
+
 root = tk.Tk()
 root.title("ein App-Beispiel")
 root.geometry("600x400")
@@ -13,12 +18,9 @@ str_var=tk.StringVar()
 
 label = tk.Label(root, text="drücke ein Button")
 label.grid(row=0,column=0)
-
 entry = tk.Entry(root,textvariable = str_var, font=('calibre',10,'normal'))
 entry.grid(row=0,column=1)
-
 button = tk.Button(root, text="drücke mich", command=on_button_click)
 button.grid(row=2,column=1)
  
 root.mainloop()
-
